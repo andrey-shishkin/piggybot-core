abf785896e1f688b1a9a7d2d1c84fe3f08161849
Andrei Shishkin <ashishkin@edmunds.com>
2022-06-02 15:47:50 +0400

    Minor fix

--- common-core/src/main/java/ru/piggybox/core/bot/common/command/delegator/CommandDelegator.java
+++ common-core/src/main/java/ru/piggybox/core/bot/common/command/delegator/CommandDelegator.java
@@ -3,7 +3,7 @@ package ru.piggybox.core.bot.common.command.delegator;
 import ru.piggybox.core.bot.common.command.BotCommand;
 import ru.piggybox.core.bot.common.controller.CommandController;
 
-public interface CommandDelegator<I, O, T extends CommandController<I, O>, C extends BotCommand<I, O, T>> {
+public interface CommandDelegator<I, O, T extends CommandController<I, O>, C extends BotCommand<I, O>> {
 
     O delegate(I request);
 
