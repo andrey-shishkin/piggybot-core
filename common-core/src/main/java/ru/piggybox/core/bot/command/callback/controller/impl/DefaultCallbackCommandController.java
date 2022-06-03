881b00719c71d02728b835df85892e8f6a7165d8
Andrei Shishkin <ashishkin@edmunds.com>
2022-06-03 16:39:50 +0400

    Deleted redundant controllers

--- common-core/src/main/java/ru/piggybox/core/bot/command/callback/controller/impl/DefaultCallbackCommandController.java
+++
@@ -1,8 +0,0 @@
-package ru.piggybox.core.bot.command.callback.controller.impl;
-
-import org.springframework.stereotype.Component;
-import ru.piggybox.core.bot.command.callback.controller.CallbackCommandController;
-
-@Component
-public class DefaultCallbackCommandController implements CallbackCommandController {
-}
--- common-core/src/main/java/ru/piggybox/core/bot/command/inline/controller/impl/DefaultInlineCommandController.java
+++
@@ -1,8 +0,0 @@
-package ru.piggybox.core.bot.command.inline.controller.impl;
-
-import org.springframework.stereotype.Component;
-import ru.piggybox.core.bot.command.inline.controller.InlineCommandController;
-
-@Component
-public class DefaultInlineCommandController implements InlineCommandController {
-}
