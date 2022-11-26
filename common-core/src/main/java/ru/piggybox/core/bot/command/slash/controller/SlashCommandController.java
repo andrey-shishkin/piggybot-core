b8b7b41276216dbbd3f116a1e280f4d339ea623f
Andrei Shishkin <compilator@yandex.ru>
2022-11-26 17:21:07 +0400

    Add default method

--- common-core/src/main/java/ru/piggybox/core/bot/command/slash/controller/SlashCommandController.java
+++ common-core/src/main/java/ru/piggybox/core/bot/command/slash/controller/SlashCommandController.java
@@ -1,8 +1,32 @@
 package ru.piggybox.core.bot.command.slash.controller;
 
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import ru.piggybox.core.bot.command.slash.dto.BotSlashRequest;
 import ru.piggybox.core.bot.command.slash.dto.BotSlashResponse;
 import ru.piggybox.core.bot.common.controller.CommandController;
 
+import java.io.Serializable;
+
 public interface SlashCommandController extends CommandController<BotSlashRequest, BotSlashResponse> {
+
+    default BotSlashResponse buildResponse(BotSlashRequest request, String commandMenu, String messageText, InlineKeyboardMarkup markup) {
+        return new BotSlashResponse() {
+            @Override
+            public String getCommand() {
+                return commandMenu;
+            }
+
+            @Override
+            public BotApiMethod<? extends Serializable> getMethod() {
+                return SendMessage.builder()
+                        .text(messageText)
+                        .chatId(request.getUser().getId().toString())
+                        .replyMarkup(markup)
+                        .build();
+            }
+        };
+    }
+
 }
